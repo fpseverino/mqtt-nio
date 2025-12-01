e5059a720ae26a4968beadc381736614092028d8
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-12-01 17:08:21 +0100

    Serialize MQTTNewConnectionTests

--- Tests/MQTTNIOTests/MQTTNewConnectionTests.swift
+++ Tests/MQTTNIOTests/MQTTNewConnectionTests.swift
@@ -27,7 +27,7 @@ import NIOTransportServices
 import NIOSSL
 #endif
 
-@Suite("MQTTNewConnection Tests")
+@Suite("MQTTNewConnection Tests", .serialized)
 struct MQTTNewConnectionTests {
     static let hostname = ProcessInfo.processInfo.environment["MOSQUITTO_SERVER"] ?? "localhost"
 
